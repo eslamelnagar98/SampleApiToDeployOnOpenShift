2a0331e11f68e9088d085e9631baa8fe03b57076
Islam <eslamelnagar44@gmail.com>
2021-08-22 10:17:40 +0200

    Add project files.

---
+++ SampleApiToDeployOnOpenShift/Controllers/ComputationsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleApiToDeployOnOpenShift.Controllers
+{
+    [ApiController]
+    [Route("api/computations")]
+    public class ComputationsController : ControllerBase
+    {
+
+        [HttpGet("{firstNumber:int}/{secondNumber:int}")]
+        public ActionResult<int> AddTwoNumbers(int firstNumber, int secondNumber)
+            => firstNumber + secondNumber;
+        
+    }
+}
